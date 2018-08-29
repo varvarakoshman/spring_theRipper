2817da7be964644363d03e4f0b6fa803cf438cad
vkoschman <207955@niuitmo.ru>
2018-08-29 15:24:11 +0400

    two phase constructor added

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -2,11 +2,23 @@ package quoters;
 
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
     @Setter
     private String message;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter(){
+        System.out.println("Phase 1");
+    }
 
     public void sayQuote() {
         for (int i = 0; i < repeat; i++) {
