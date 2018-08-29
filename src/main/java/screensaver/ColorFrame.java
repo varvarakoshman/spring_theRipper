03e5c3df661aa6264e7e27bd547c09d5bc645647
vkoschman <207955@niuitmo.ru>
2018-08-29 20:46:15 +0400

    random color frame

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,26 @@
+package screensaver;
+
+import javax.swing.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.Random;
+
+@Component
+public class ColorFrame extends JFrame {
+    @Autowired
+    private Color color;
+    public ColorFrame(){
+        setSize(200,200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace(){
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+}
