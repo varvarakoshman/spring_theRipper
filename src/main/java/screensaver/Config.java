d6e47faf418caaf267a30609ee8aab88a5cc3e0d
vkoschman <207955@niuitmo.ru>
2018-08-29 21:42:09 +0400

    Custom scope example

--- src/main/java/screensaver/Config.java
+++ src/main/java/screensaver/Config.java
@@ -10,7 +10,7 @@ import java.util.Random;
 @ComponentScan(basePackages = "screensaver")
 public class Config {
     @Bean
-    @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+    @Scope("periodical")
     public Color color(){
         Random random = new Random();
         return new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255));
